c712d2ee9078fbc7f2ce7afb72da365a08faf03e
exiqa <sarmat@ukr.net>
2013-02-27 21:56:40 +0200

    some changes

--- BookDownloader/src/com/blogspot/magazsa/web/bkdwnldr/BookDownloader.java
+++ BookDownloader/src/com/blogspot/magazsa/web/bkdwnldr/BookDownloader.java
@@ -3,11 +3,13 @@ package com.blogspot.magazsa.web.bkdwnldr;
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * This program provides easy way to download books from http://many-books.org
@@ -22,11 +24,16 @@ import java.net.URL;
 public class BookDownloader {
 
 	private static final String PATH = "http://www.many-books.org";
-	
-	public BookDownloader() {}
-	
+	private static final String DOWNLOAD_PATH = PATH + "/download";
+
+	private String downloadsDir = "/Downloads";
+
+	public BookDownloader() {
+	}
+
 	/**
 	 * Downloads and saves file from remote url
+	 * 
 	 * @param url
 	 * @param file
 	 */
@@ -43,7 +50,55 @@ public class BookDownloader {
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
+		} finally {
+			if (in != null) {
+				try {
+					in.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			if (out != null) {
+				try {
+					out.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
 		}
 	}
 
+	public void setDownloadsDir(String downloadsDir) {
+		this.downloadsDir = downloadsDir;
+	}
+
+	public String getLinkByFirstLetter(String letter) {
+		Pattern pattern = Pattern
+				.compile("(?m)(?i)(?u)(?s).*<a\\s+href\\s*=\\s*\"(.+?)\"\\s*>"
+						+ letter.toUpperCase() + "</a>.*");
+		BufferedReader reader = null;
+		try {
+			reader = new BufferedReader(new InputStreamReader(
+					new URL(PATH).openStream()));
+			String line;
+			Matcher m;
+			while ((line = reader.readLine()) != null) {
+				m = pattern.matcher(line);
+				if (m.matches()) {
+					return PATH + m.group(1);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (reader != null) {
+				try {
+					reader.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return null;
+	}
 }
