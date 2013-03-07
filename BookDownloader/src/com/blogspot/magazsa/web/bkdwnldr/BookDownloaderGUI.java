5ed7f717ced48d3062b242267e6ed87af8bc3cf1
exiqa <sarmat@ukr.net>
2013-03-07 18:31:34 +0200

    adding progress bar

--- BookDownloader/src/com/blogspot/magazsa/web/bkdwnldr/BookDownloaderGUI.java
+++ BookDownloader/src/com/blogspot/magazsa/web/bkdwnldr/BookDownloaderGUI.java
@@ -6,11 +6,16 @@ import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
@@ -27,6 +32,7 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JProgressBar;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
@@ -40,13 +46,13 @@ public class BookDownloaderGUI extends JFrame {
 
 	private BookDownloader downloader;
 	private File downloadDir;
-	
+
 	private List<Author> authors;
 	private List<Book> books;
 
 	private DefaultListModel<String> authorListModel;
 	private DefaultListModel<String> booksListModel;
-	
+
 	private JList<String> authorList;
 	private JList<String> booksList;
 
@@ -56,22 +62,28 @@ public class BookDownloaderGUI extends JFrame {
 	private JButton showButton;
 	private JButton downloadButton;
 	private JButton downloadAllButton;
-	
+
 	private JMenuBar menuBar;
+
+	private JProgressBar progressBar;
 
 	public BookDownloaderGUI() {
 		setTitle("Book Downloader");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		downloader = new BookDownloader();
 		downloadDir = new File(downloader.getDownloadDirPath());
-		if(!downloadDir.exists()) {
+		if (!downloadDir.exists()) {
 			downloadDir.mkdir();
 		}
-		
+
 		menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
-		
+
+		progressBar = new JProgressBar(0, 100);
+		progressBar.setValue(0);
+		progressBar.setStringPainted(true);
+
 		JMenu programMenu = new JMenu("Опции");
 		JMenuItem preferencesMenuItem = new JMenuItem("Настройки");
 		preferencesMenuItem.addActionListener(new ActionListener() {
@@ -80,7 +92,8 @@ public class BookDownloaderGUI extends JFrame {
 				preferences.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 				preferences.setLayout(new FlowLayout(FlowLayout.LEFT));
 				preferences.add(new JLabel("Папка для загрузок: "));
-				final JTextField path = new JTextField(downloadDir.getPath(), 20);
+				final JTextField path = new JTextField(downloadDir.getPath(),
+						20);
 				JButton browseButton = new JButton("Изменить");
 				browseButton.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
@@ -102,7 +115,7 @@ public class BookDownloaderGUI extends JFrame {
 		});
 		programMenu.add(preferencesMenuItem);
 		menuBar.add(programMenu);
-		
+
 		logger = new JTextArea(3, 35);
 		logger.setFont(new Font("Monospaced", Font.PLAIN, 11));
 		logger.setEditable(false);
@@ -143,7 +156,7 @@ public class BookDownloaderGUI extends JFrame {
 		JPanel topPanel = new JPanel();
 		topPanel.setLayout(new BorderLayout());
 		topPanel.setBorder(BorderFactory.createEmptyBorder(5, 4, 5, 4));
-		topPanel.add(new JLabel("Поиск по автору: "), BorderLayout.WEST);
+		topPanel.add(new JLabel("Поиск по автору:  "), BorderLayout.WEST);
 
 		// down panel
 		JPanel downPanel = new JPanel();
@@ -151,7 +164,7 @@ public class BookDownloaderGUI extends JFrame {
 
 		// buttons panel
 		JPanel buttonsPanel = new JPanel();
-		buttonsPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+		buttonsPanel.setLayout(new GridBagLayout());
 
 		final JTextField searchField = new JTextField(25);
 		searchField.addFocusListener(new FocusAdapter() {
@@ -182,7 +195,9 @@ public class BookDownloaderGUI extends JFrame {
 				showBooks(author);
 			}
 		});
-		buttonsPanel.add(showButton);
+		buttonsPanel.add(showButton, new GridBagConstraints(0, 0, 1, 1, 0, 0,
+				GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(
+						5, 0, 5, 5), 0, 0));
 
 		downloadButton = new JButton("Скачать");
 		downloadButton.addActionListener(new ActionListener() {
@@ -199,27 +214,35 @@ public class BookDownloaderGUI extends JFrame {
 					return;
 				}
 				List<Book> booksToDownload = new ArrayList<>();
-				for (int i = 0; i < booksIndices.length; i++) {
+				for (int i : booksIndices) {
 					booksToDownload.add(books.get(i));
 				}
-				downloadFiles(booksToDownload);
+				new DownloadTask(booksToDownload).execute();
 				booksList.clearSelection();
 			}
 		});
-		buttonsPanel.add(downloadButton);
+		buttonsPanel.add(downloadButton, new GridBagConstraints(1, 0, 1, 1, 0,
+				0, GridBagConstraints.CENTER, GridBagConstraints.NONE,
+				new Insets(5, 0, 5, 5), 0, 0));
 
 		downloadAllButton = new JButton("Скачать все");
 		downloadAllButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				if (books != null) {
-					downloadAllFiles(books);
+					new DownloadTask(books).execute();
 				} else {
 					JOptionPane.showMessageDialog(BookDownloaderGUI.this,
 							"Список книг пуст!");
 				}
 			}
 		});
-		buttonsPanel.add(downloadAllButton);
+		buttonsPanel.add(downloadAllButton, new GridBagConstraints(2, 0, 1, 1,
+				0, 0, GridBagConstraints.CENTER, GridBagConstraints.NONE,
+				new Insets(5, 0, 5, 5), 0, 0));
+
+		buttonsPanel.add(progressBar, new GridBagConstraints(0, 1, 3, 1, 0, 0,
+				GridBagConstraints.EAST, GridBagConstraints.HORIZONTAL,
+				new Insets(0, 0, 0, 5), 0, 0));
 
 		downPanel.add(buttonsPanel, BorderLayout.WEST);
 		downPanel.add(new JScrollPane(logger,
@@ -246,7 +269,7 @@ public class BookDownloaderGUI extends JFrame {
 				findButton.setEnabled(false);
 				logger.append("Подождите.....идет поиск автора\n");
 				return downloader.getAuthorsByName(authorName);
-			};
+			}
 
 			protected void done() {
 				try {
@@ -265,7 +288,7 @@ public class BookDownloaderGUI extends JFrame {
 					setCursor(null);
 					findButton.setEnabled(true);
 				}
-			};
+			}
 
 		}.execute();
 	}
@@ -286,62 +309,16 @@ public class BookDownloaderGUI extends JFrame {
 						booksListModel.addElement(book.getTitle());
 					}
 				} catch (Exception e) {
-					logger.append("Проблемы с подключением к серверу. Попробуйте еще раз");
+					logger.append("Проблемы с подключением к серверу. Попробуйте еще раз\n");
 				} finally {
 					setCursor(null);
 					showButton.setEnabled(true);
 				}
-			};
+			}
 
 		}.execute();
 	}
 
-	private void downloadFiles(final List<Book> books) {
-		new SwingWorker<Void, Void>() {
-			protected Void doInBackground() throws Exception {
-				downloadButton.setEnabled(false);
-				downloadAllButton.setEnabled(false);
-				File authorDir = createAuthorDir(authorList.getSelectedValue());
-				for (Book book : books) {
-					logger.append("Загрузка книги..." + book.getTitle() + "...");
-					downloader.download(authorDir, book);
-					logger.append("done!\n");
-				}
-				return null;
-			};
-
-			protected void done() {
-				downloadButton.setEnabled(true);
-				downloadAllButton.setEnabled(true);
-				logger.append("Загрузка завершена!\n");
-			};
-
-		}.execute();
-	}
-
-	private void downloadAllFiles(final List<Book> books) {
-		new SwingWorker<Void, Void>() {
-			protected Void doInBackground() throws Exception {
-				downloadButton.setEnabled(false);
-				downloadAllButton.setEnabled(false);
-				File authorDir = createAuthorDir(authorList.getSelectedValue());
-				for (Book book : books) {
-					logger.append("Загрузка книги..." + book.getTitle() + "...");
-					downloader.download(authorDir, book);
-					logger.append("done!\n");
-				}
-				return null;
-			};
-
-			protected void done() {
-				downloadButton.setEnabled(true);
-				downloadAllButton.setEnabled(true);
-				logger.append("Загрузка завершена!\n");
-			};
-
-		}.execute();
-	}
-	
 	private File createAuthorDir(String authorName) {
 		File authorDir = new File(downloader.getDownloadDirPath(), authorName);
 		if (!authorDir.exists()) {
@@ -349,7 +326,7 @@ public class BookDownloaderGUI extends JFrame {
 		}
 		return authorDir;
 	}
-	
+
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 
@@ -358,6 +335,51 @@ public class BookDownloaderGUI extends JFrame {
 				new BookDownloaderGUI().setVisible(true);
 			}
 		});
+
+	}
+
+	private class DownloadTask extends SwingWorker<Void, Void> implements
+			PropertyChangeListener {
+
+		private List<Book> books;
+
+		public DownloadTask(List<Book> books) {
+			this.books = books;
+			addPropertyChangeListener(this);
+		}
+
+		protected Void doInBackground() throws Exception {
+			downloadButton.setEnabled(false);
+			downloadAllButton.setEnabled(false);
+			progressBar.setValue(0);
+			File authorDir = createAuthorDir(authorList.getSelectedValue());
+			int progressTick = 100 / books.size();
+			int progress = 0;
+			setProgress(progress);
+			for (Book book : books) {
+				logger.append("Загрузка книги..." + book.getTitle() + "...");
+				downloader.download(authorDir, book);
+				logger.append("done!\n");
+				progress += progressTick;
+				setProgress(progress);
+			}
+			return null;
+		}
+
+		protected void done() {
+			downloadButton.setEnabled(true);
+			downloadAllButton.setEnabled(true);
+			logger.append("Загрузка завершена!\n");
+			progressBar.setValue(100);
+		}
+
+		@Override
+		public void propertyChange(PropertyChangeEvent evt) {
+			if ("progress" == evt.getPropertyName()) {
+				Integer progress = (Integer) evt.getNewValue();
+				progressBar.setValue(progress);
+			}
+		}
 
 	}
 
